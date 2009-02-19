0dfd557b590778677cbe3d7606a46d58dd367fd3
Eric Weinert <drake@hungrychild.org>
2009-02-18 18:27:10 -1000

    named scope: wants_daily_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   
+  validates_presence_of :email
+  validates_uniqueness_of :email, :message => "has already been signed up"
+  
   # perhaps in the future this will have an opt-in flag or something, for now, everybody wants it
   named_scope :wants_daily_email
   
