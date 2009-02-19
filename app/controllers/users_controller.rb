a3e845f4dad6d619d6c12cefd0a91d73c9199c5d
Eric Weinert <drake@hungrychild.org>
2009-02-18 18:28:34 -1000

    sign up for emails!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,8 +44,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        flash[:notice] = 'User was successfully created.'
-        format.html { redirect_to(@user) }
+        flash[:notice] = "#{@user.name} successfully signed up to receive daily emails"
+        format.html { redirect_to('/') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
